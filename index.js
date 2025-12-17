f9297faa68953d3ee1bb1677659190a9f1bad2e2
j8ls <mavrina1007@gmail.com>
2025-12-17 17:05:05 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import { Telegraf } from "telegraf";
+
+const token = process.env.BOT_TOKEN;
+if (!token) throw new Error("BOT_TOKEN is not set");
+
+const bot = new Telegraf(token);
+
+const replyText =
+  'К сожалению, данное исследование уже закрыто.\n' +
+  'Но вы можете написать свои пожелания и предложения по темам для будущих постов!\n' +
+  'Я обязательно их рассмотрю! Просто напишите ваше сообщение здесь!';
+
+bot.start((ctx) => ctx.reply(replyText));
+bot.on("text", (ctx) => ctx.reply(replyText));
+bot.on("message", (ctx) => ctx.reply(replyText)); // на случай не-текстовых сообщений
+
+bot.launch();
+
+// Корректное завершение на Railway/в контейнере
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
